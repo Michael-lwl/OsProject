31da6d8deedd923c72b80e4339fa8bb3a369fa64
Hikikonova <97136013+Hikikonova@users.noreply.github.com>
2024-09-02 21:25:31 +0200

    Update mbr.h

--- include/core/mbr.h
+++ include/core/mbr.h
@@ -3,12 +3,12 @@
 #include <iostream>
 #include <fstream>
 #include <cstring>
-#include "../include/core/system.h"
-#include "../include/core/data_impl.h"
-#include "../include/core/data.h"
-#include "../include/array.h"
-#include "../include/feature/fat/bs_system.h"
-#include "../include/feature/inode/inode_system.h"
+#include "./system.h"
+#include "./data_impl.h"
+#include "./data.h"
+#include "../array.h"
+#include "../feature/fat/bs_system.h"
+#include "../feature/inode/inode_system.h"
 
 const int BOOT = 0x0000;
 const int MBR_SIZE = 512;
@@ -23,6 +23,11 @@ const int maxHeads = 256;
 const int maxCylinders = 1024;
 const int blockSize = 512;
 
+enum SpeicherSystem {
+BS_FAT,
+INODE
+};
+
 struct CHS {
     unsigned int c : 10;
     unsigned char h : 6;
@@ -30,30 +35,32 @@ struct CHS {
 };
 
 struct partition {
-   char isBootable;
-   char type;
-   //unsigned int length;
-  unsigned int sectorsCount;
-   CHS* firstSektor;
-   CHS* lastSektor;
-   CHS* startPtr;
-   System* system;
+    char isBootable;
+    char type;
+    //unsigned int length;
+    unsigned int sectorsCount;
+    CHS* firstSektor;
+    CHS* lastSektor;
+    CHS* startPtr;
+    System* system;
 };
 
 class Master_Boot_Record{
 public:
 
-Master_Boot_Record(unsigned int Speicher, char System){
-createPartition(Speicher,1);
+Master_Boot_Record(unsigned int Blöcke, SpeicherSystem System){
+this->Blöcke = Blöcke;
+this->System = System;
+
 }
-void createPartition(unsigned int Speicher, char System){
+void createPartition(unsigned int Speicher, SpeicherSystem System){
 partition Eintrag;
 Data *dataHandler = new Data_Impl(blockSize);
 Eintrag.isBootable = 1;
 Eintrag.type = 1;
-Eintrag.firstSektor = createSector(getSectorCount());
+Eintrag.firstSektor = createSector(1); //Todo First Sektor ermitteln
 Eintrag.lastSektor = createSector(Speicher);
-Eintrag.sectorsCount = getSectorCount();
+Eintrag.sectorsCount = getSectorCount(Eintrag);
     if(Partitions[0].firstSektor != Partitions[0].startPtr ){
         Eintrag.startPtr = Eintrag.firstSektor;
     }
@@ -61,39 +68,36 @@ Eintrag.sectorsCount = getSectorCount();
     Eintrag.startPtr = Partitions[0].startPtr;
     }
 Eintrag.system = createSystem(System,Speicher,dataHandler);
+
+//
+for(int i = 0; i < 4; i++){
+if(Partitions[i].firstSektor == NULL || Partitions[i].lastSektor == NULL){
+ Partitions[i] = Eintrag;
+ break;
+ }
+}
 }
 
-System*  createSystem(char System, unsigned int Speicher,Data* datahandler){
-if(System == 1){return bootBSFat(blockSize,Speicher,datahandler);}
-else if(System = 2){return bootINode(blockSize,Speicher,datahandler);}
+System*  createSystem(SpeicherSystem System, unsigned int Speicher,Data* datahandler){
+if(System == BS_FAT){return bootBSFat(blockSize,Speicher,datahandler);}
+//else if(System = INODE){return bootINode(blockSize,Speicher,datahandler);}
 else{throw("Das System konnte nicht erstellt werden");}
 }
 
-unsigned int getSectorCount(){
-int SectorCount = 0;
-for(int i = 0; i < 4; i++){
-if(Partitions[i].firstSektor->c != 0 || Partitions[i].firstSektor->h != 0 || Partitions[i].firstSektor->s != 0){
-SectorCount++;
-}
-if(Partitions[i].lastSektor->c != 0 || Partitions[i].lastSektor->h != 0 || Partitions[i].lastSektor->s != 0){
-SectorCount++;
-}
+unsigned int getSectorCount(partition E){
 
-}
-return SectorCount;
-}
+return (E.lastSektor->c * maxHeads + E.lastSektor->h) * maxSectors + ( E.lastSektor->s - 1);
+};
+
 
     CHS* createSector(unsigned int block) {
     CHS* sector;
 
-    const int maxSectors = 63;        // Sektoren reichen von 1 bis 63
-    const int maxHeads = 256;         // Köpfe reichen von 0 bis 255
-    const int maxCylinders = 1024;    // Zylinder reichen von 0 bis 1023
 
-    // Maximale Anzahl an Blöcken im gegebenen Bereich
+
     const unsigned int maxBlocks = maxCylinders * maxHeads * maxSectors;
 
-    if (block > maxBlocks) {
+    if (block >= maxBlocks) {
         throw std::out_of_range("Der gewünschte Index ist außerhalb unseres Speicherplatzes");
     }
 
@@ -105,26 +109,32 @@ return SectorCount;
     sector->c = block;
 
     return sector;
+
 }
 
 
-//Todo Inputs für memorySize und BlockSize für die händische Abfrage einfügen
 bool boot(int bIndex = 0){
 if(Partitions[bIndex].isBootable != 1){
 return Partitions[bIndex].isBootable != 1 && Partitions[bIndex].system->boot();
 }
-return 1;
-}
+return 1;}
+
 
     BsFat* bootBSFat(unsigned int blockSize,unsigned int memorySize, Data *dataHandler){
     BsFat* B = BsFat::create(memorySize,blockSize, dataHandler);
     return B;
     }
 
-      INode* bootINode(int blockSize, int memorySize, Data *dataHandler){
-       INode* I = INode::create(memorySize,blockSize, dataHandler);
-      return I;
-     }
+
+
+
+//   INode* bootINode(int blockSize, int memorySize, Data *dataHandler{
+
+//    INode* I = INode::create(memorySize,blockSize, dataHandler;
+
+//   return;
+
+//  }
 
 
     //Getter
@@ -138,6 +148,9 @@ private:
     partition Partitions[4];
     unsigned int diskSignature; //normalerweise nur in Windows
     const int identificationCode = 0xAA55;
+    unsigned int Blöcke;
+    SpeicherSystem System;
+
 
 };
 
